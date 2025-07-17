94300d3b3eef0b9ce29763262bd4981b6ec958e3
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-07-17 19:47:11 +0530

    final

--- react-3/effect/src/User.jsx
+++ react-3/effect/src/User.jsx
@@ -6,7 +6,7 @@ function User() {
     const [error, setError]=useState(null)
     const [loading, setLoading]=useState(true)
     const [user, setUser]=useState({})
-
+    const [count, setCount]=useState(0)
 
     useEffect(()=>{
         //make api request
@@ -21,8 +21,21 @@ function User() {
                 setError(error)
             }
         }
-        console.log("hello")
+
+        //example of why we use cleanup function (because to remove this timer if useeffect re-run)
+        const timer=setInterval(() => {
+            console.log("timer ran")
+            setCount((prevcount)=>prevcount+1)
+        },1000)
+
+        console.log("calling from useEffect function")
         fetchData()
+
+        //this cleanup function will run just before useEffect is re-run
+        return ()=>{
+            console.log("calling fron cleanup function")
+            clearInterval(timer)
+        }
     },[])
 
     if(error){
@@ -30,7 +43,10 @@ function User() {
     }
 
     return (
-        <div>{loading ? "Loading" : user.name}</div>
+        <>
+            <div>{loading ? "Loading" : user.name}</div>
+            <div>timer : {count} sec</div>
+        </>
     )
 }
 
